8433f87c0601d00a85e4360eb59c447b33babf05
Kacaii <kacaii.privado@gmail.com>
2025-08-09 09:53:10 -0300

    add error handling for the deletion

--- desafio_i/src/desafio_i/main.py
+++ desafio_i/src/desafio_i/main.py
@@ -1,15 +1,9 @@
+from json.decoder import JSONDecodeError
 import os
 import sys
 import json
 from pathlib import Path
 from typing import TypedDict
-
-
-class Person(TypedDict):
-    name: str
-    phone: str
-    email: str
-
 
 help_message = """
 Usage:
@@ -23,6 +17,13 @@ Args:
 """
 
 
+class Person(TypedDict):
+    name: str
+    phone: str
+    email: str
+
+
+## Starting point.
 def main():
     if len(sys.argv) < 3:
         print(help_message)
@@ -63,10 +64,11 @@ def handle_remove(json_path: str):
     )
 
     match remove_person_from_json(json_path, email):
+        # I could use an `if else`, but I REALLY like switch statements.
         case True:
-            print(email, "was removed successfully from the list! <3")
+            print(email, "was removed successfully from the list! ")
         case False:
-            print("Sorry, but I couldn't find", email, "on the list.")
+            print(email, "was not removed on the list. 󱆥")
 
 
 def remove_person_from_json(json_path: str, email: str) -> bool:
@@ -78,19 +80,38 @@ def remove_person_from_json(json_path: str, email: str) -> bool:
     current_dir = Path().cwd()
     data_path = os.path.join(current_dir, json_path)
 
-    with open(data_path, "r") as file:
-        data: list[Person] = json.load(file)
+    # Error handling is important.
+    try:
+        with open(data_path, "r") as file:
+            data: list[Person] = json.load(file)
+    except JSONDecodeError:
+        print("Failed to decode data from the json file. 󰘦")
+        return False
+    except FileNotFoundError:
+        print("Failed to locate the data file. 󱔼")
+        return False
 
+    # Filter everyone that doesnt have the passed email string.
     updated_data = [person for person in data if person.get("email") != email]
 
+    # Run this if someone got filtered.
     if len(updated_data) < len(data):
-        with open(data_path, "w") as file:
-            json.dump(updated_data, file)
+        try:
+            with open(data_path, "w") as file:
+                json.dump(updated_data, file)
 
-        # Return true if the person was removed successfully.
-        return True
+            # Return true if the person was removed successfully.
+            return True
+        except JSONDecodeError:
+            print("Failed to decode data from the json file. 󰘦")
+            return False
+        # Just in case someone inputs a file that doesnt exist.
+        except FileNotFoundError:
+            print("Failed to locate the data file. 󱔼")
+            return False
 
     # Return false if the person was not removed.
+    print("The email is not on the list, maybe you missed a character?")
     return False
 
 
