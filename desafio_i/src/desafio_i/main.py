bb3e477859b3a6cb9efd8efa67efcdd2a0bcebd4
Kacaii <kacaii.privado@gmail.com>
2025-08-09 09:32:39 -0300

    handle removing person fom the list

--- desafio_i/src/desafio_i/main.py
+++ desafio_i/src/desafio_i/main.py
@@ -1,24 +1,97 @@
 import os
+import sys
 import json
-from dataclasses import dataclass
 from pathlib import Path
+from typing import TypedDict
 
 
-@dataclass
-class Person:
+class Person(TypedDict):
     name: str
     phone: str
     email: str
 
 
+help_message = """
+Usage:
+    
+    $ <python> src/desafio_i/main.py [add,del] <json path>
+
+Args:
+
+    add     add a person to the list
+    del     removes a person from the list
+"""
+
+
 def main():
+    if len(sys.argv) < 3:
+        print(help_message)
+
+        # Returns safely if there is no args or json path.
+        return
+
+    json_path = sys.argv[2]
+
+    match sys.argv[1]:
+        case "del":
+            handle_remove(json_path)
+        case _:
+            print(help_message)
+
+
+def append_person_to_json(json_path: str, person: Person):
+    """
+    Opens an arbitrary `json` file using the current diretory as reference and
+    appends a person to it
+    """
+
     current_dir = Path().cwd()
-    data_path = os.path.join(current_dir, "src/data.json")
+    data_path = os.path.join(current_dir, json_path)
 
-    with open(data_path) as file:
-        data = json.load(file)
+    with open(data_path, "r") as file:
+        data: list[Person] = json.load(file)
 
-    print(data)
+        data.append(person)
+
+        file.seek(0)
+        json.dump(data, file)
+
+
+def handle_remove(json_path: str):
+    email = input(
+        "Please enter the email you would like to remove from the list:\n  -> "
+    )
+
+    match remove_person_from_json(json_path, email):
+        case True:
+            print(email, "was removed successfully from the list! <3")
+        case False:
+            print("Sorry, but I couldn't find", email, "on the list.")
+
+
+def remove_person_from_json(json_path: str, email: str) -> bool:
+    """
+    Opens an arbitrary `json` file using the current diretory as reference and
+    removes a person from it by their email
+    """
+
+    current_dir = Path().cwd()
+    data_path = os.path.join(current_dir, json_path)
+
+    with open(data_path, "r") as file:
+        data: list[Person] = json.load(file)
+
+    updated_data = [person for person in data if person.get("email") != email]
+
+    if len(updated_data) < len(data):
+        with open(data_path, "w") as file:
+            json.dump(updated_data, file)
+
+        # Return true if the person was removed successfully.
+        return True
+
+    # Return false if the person was not removed.
+    return False
 
 
 # Starting execution 
