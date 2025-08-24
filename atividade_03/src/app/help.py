88ae94343ab76cb8ac3ec759440796f2abec9141
Kacaii <kacaii.privado@gmail.com>
2025-08-23 21:10:08 -0300

    add help message

---
+++ atividade_03/src/app/help.py
@@ -0,0 +1,13 @@
+def main():
+    msg = """
+    Usage:
+
+        poetry run help                     Display this help message
+        poetry run normal_distribution      Display the normal distribution plot
+    """
+
+    print(msg)
+
+
+if __name__ == "__main__":
+    main()
