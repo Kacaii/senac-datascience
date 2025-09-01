35a9c2ace77fcf55dd16f28e6af443b0d0dcf811
Kacaii <kacaii.privado@gmail.com>
2025-09-01 17:41:55 -0300

    finish kde

--- atividade_04/src/app/main.py
+++ atividade_04/src/app/main.py
@@ -1,7 +1,7 @@
 import seaborn as sns
-from sklearn.preprocessing import LabelEncoder
 import matplotlib.pyplot as plt
 import polars as pl
+# from sklearn.preprocessing import LabelEncoder
 
 # survived      Survival                0 = No, 1 = Yes
 # pclass        Ticket class            1 = 1st, 2 = 2nd, 3 = 3rd
@@ -17,10 +17,14 @@ import polars as pl
 
 def main():
     df = pl.scan_parquet("src/train.parquet")
-    sns.set_theme(style="ticks", palette="deep")
-    le = LabelEncoder()
+    sns.set_theme(style="ticks")
 
-    plt.show()
+    survived_with_age_col = df.filter(pl.col("survived") == 1).select(
+        pl.col("age").fill_nan(pl.mean("age"))
+    )
+
+    sns.histplot(data=survived_with_age_col.collect(), x="age", kde=True)
+    plt.savefig("src/graphs/survived_by_age.")
 
 
 if __name__ == "__main__":
